df0c3090e7e1f3f6cd7ce324c01dcf004fb98579
ibiscum <ulf.luecking@gmail.com>
2025-12-08 18:24:00 +0100

    ch01

--- chapter-01/exercise-01.05/person-sort.ts
+++ chapter-01/exercise-01.05/person-sort.ts
@@ -1,7 +1,7 @@
 // The interface for the person objects
 interface Person {
-    firstName: string;
-    lastName: string;
+  firstName: string;
+  lastName: string;
 }
 
 // we use this variable to count calls
@@ -9,8 +9,8 @@ let count = 0;
 
 // Gets the full name of a given person
 function getFullName (person: Person) {
-    count += 1;
-    return `${person.firstName} ${person.lastName}`;
+  count += 1;
+  return `${person.firstName} ${person.lastName}`;
 }
 
 // array of persons that we need to sort
@@ -25,28 +25,30 @@ const programmers: Person[] = [
     // feel free to add as many as you want
 ];
 
-
 // a naive and straightforward sorting function
 function naiveSortPersons (persons: Person[]): Person[] {
-    return persons.slice().sort((first, second) => {
-        const firstFullName = getFullName(first);
-        const secondFullName = getFullName(second);
-        return firstFullName.localeCompare(secondFullName);
-    })
+  return persons.slice().sort((first, second) => {
+    const firstFullName = getFullName(first);
+    const secondFullName = getFullName(second);
+    return firstFullName.localeCompare(secondFullName);
+  })
 }
 
 // Takes an array of persons, and return (a sorted) array of persons
 // Uses a Schwartzian transform
 function schwartzSortPersons (persons: Person[]): Person[] {
-    // Use the array's `map` function to transform each element into a tuple
-    const tuples: [Person, string][] = persons.map(person => [person, getFullName(person)]);
-    // Sort the `tuples` array of tuples, using the standard `sort` method, on the second element of the tuple
-    tuples.sort((first, second) => first[1].localeCompare(second[1]));
-    // Transform the sorted array of tuples into the format we want - just an array of `person` objects
-    // by taking the first element of each tuple, discarding the schwartz.
-    const result = tuples.map(tuple => tuple[0]);
-    // Return the new array from the function
-    return result;
+  // Use the array's `map` function to transform each element into a tuple
+  const tuples: [Person, string][] = persons.map(person => [person, getFullName(person)]);
+
+  // Sort the `tuples` array of tuples, using the standard `sort` method, on the second element of the tuple
+  tuples.sort((first, second) => first[1].localeCompare(second[1]));
+
+  // Transform the sorted array of tuples into the format we want - just an array of `person` objects
+  // by taking the first element of each tuple, discarding the schwartz.
+  const result = tuples.map(tuple => tuple[0]);
+
+  // Return the new array from the function
+  return result;
 }
 
 // Call the naive `sortPersons` function on our defined array
--- chapter-03/exercise-03.01/exercise.ts
+++ chapter-03/exercise-03.01/exercise.ts
@@ -6,9 +6,9 @@ for(let i = 0; i < values.length; i++) {
 const average = total/values.length;
 console.log(average);
 
-// Here is the code that calcultates average using functions. 
+// Here is the code that calculates average using functions.
 
-//const calcAverage = (values: number[]): number => (values.reduce((prev, curr) => prev + curr, 0) / values.length);
-//const values = [8, 42, 99, 161];
-//const average = calcAverage(values);
-//console.log(average);
+// const calcAverage = (values: number[]): number => (values.reduce((prev, curr) => prev + curr, 0) / values.length);
+// const values = [8, 42, 99, 161];
+// const average = calcAverage(values);
+// console.log(average);
