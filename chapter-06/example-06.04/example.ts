ef73b6da8bf6113364e356dd0155fa282ac49328
ibiscum <ulf.luecking@gmail.com>
2026-02-24 18:34:46 +0100

    ch06

--- chapter-06/example-06.04/example.ts
+++ chapter-06/example-06.04/example.ts
@@ -1,23 +1,22 @@
 interface ErrorMessage  {
     // can only be string | number | symbol
-    [msg: number ]: string;
+    // [msg: number ]: string;
     // you can add other properties once they are of the same type
     apiId: number
 }
 // message object of Index type ErrorMessage
-const errorMessage: ErrorMessage  = {
-   0: "system error",
-   1: "overload",
+// const errorMessage: ErrorMessage  = {
+//    0: "system error",
+//    1: "overload",
+//    apiId: 12345
+// };
+
+// message object of Index type ErrorMessage
+const errorMessageA: ErrorMessage = {
+  //  '0': "system error",
+   // 1: "overload",
    apiId: 12345
 };
 
-// message object of Index type ErrorMessage
-//const errorMessage: ErrorMessage  = {
-  //  '0': "system error",
-   // 1: "overload",
-   // apiId: 12345 };
-
 // console out object
-console.log(
-    errorMessage
-);
+console.log(errorMessageA);
